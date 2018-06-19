3a481e3f0f9ef2fbce50ac2b1e9caf4b1e302d5e
davulurikoti <davulurikoti@gmail.com>
2018-06-19 00:00:26 -0500

    LoginAndGetAuthorsDetails

--- mid-intro/src/app/app.component.ts
+++ mid-intro/src/app/app.component.ts
@@ -2,8 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`<pm-authors></pm-authors>`
+  templateUrl:'./app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  loggedIn :boolean = false;
+  name :string = '';
+  error : string = '';
+  brand :string = '#DKRM';
+  getDetailsClicked :boolean = false;
+  hideDetailsClicked :boolean = false;
+  login(data) :void{
+    if(data.user_name == data.password){
+      this.loggedIn = true;
+      this.name = data.user_name;
+      this.error = '';
+    }
+    else{
+      this.loggedIn = false;
+      this.error = 'Please use same user name and password!';
+      this.name = '';
+    }
+  }
+  logout() :void{
+    this.loggedIn = false;
+    this.name = '';
+  }
+  getDetails() :void{
+    this.getDetailsClicked = true;
+    this.hideDetailsClicked = false;
+  }
+  hideDetails() :void{
+    this.hideDetailsClicked = true;
+    this.getDetailsClicked = false;
+  }
 }
